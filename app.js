afc8d8f500d26521a72f94c1fd51d9bfa6acf74b
Suvam Naskar <mail@suvamnaskar.me>
2024-08-05 21:48:16 +0530

    contact form with mongoDB

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+// const serverless = require('serverless-http');
+const path = require('path');
+const mongoose = require('mongoose');
+
+app.set('view engine', 'ejs');
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+mongoose.connect("mongodb://localhost:27017/myNewApp")
+
+const contactSchema = {
+    email: String,
+    query: String
+};
+
+const contact = mongoose.model("contact", contactSchema);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/admin', (req, res) => {
+    res.render('admin.ejs');
+})
+
+app.get('/contact', async (req, res) => {
+    res.render('contact.ejs');
+})
+
+app.post('/contact', async (req, res) => {
+    let response = await new contact({
+        email: req.body.email,
+        query: req.body.query
+    })
+    response.save();
+    res.redirect('contact')
+})
+
+app.listen(3000)
+
+// module.exports.handler = serverless(app);
---
+++ boilerplate/main.js
@@ -0,0 +1,29 @@
+/*
+    const express = require('express')
+
+    const app = express()
+    // const serverless = require('serverless-http');  // only for lambda
+    const path = require('path');
+
+    app.set('view engine', 'ejs');
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.static(path.join(__dirname, 'public')))
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+
+    app.listen(80)
+
+    // module.exports.handler = serverless(app);  // only for lambda
+
+*/
+
+
+/*
+
+    npm init
+    npm i express mongoose ejs cookie-parser
+
+ */
